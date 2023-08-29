92b72b632df7a688a760ce0e5ab260620d91a296
leeonon <v5sollee@gmail.com>
2023-08-29 16:51:44 +0800

    Initial commit

---
+++ src/routes/Header.svelte
@@ -0,0 +1,65 @@
+<script>
+	import { page } from '$app/stores';
+	import logo from '$lib/images/svelte-logo.svg';
+	import github from '$lib/images/github.svg';
+</script>
+
+<header>
+	<nav>
+		<ul>
+			<li aria-current={$page.url.pathname === '/' ? 'page' : undefined}>
+				<a href="/">Home</a>
+			</li>
+			<li aria-current={$page.url.pathname === '/about' ? 'page' : undefined}>
+				<a href="/about">About</a>
+			</li>
+		</ul>
+	</nav>
+</header>
+
+<style>
+	header {
+		position: absolute;
+		left: 0;
+	}
+
+	nav {
+		--background: rgba(255, 255, 255, 0.7);
+	}
+
+	ul {
+		padding: 0;
+		margin: 0;
+		list-style: none;
+		background: var(--background);
+		background-size: contain;
+	}
+
+	li {
+		position: relative;
+		height: 100%;
+		padding: 1em 0;
+	}
+
+	li[aria-current='page'] > a {
+		color: var(--color-theme-1);
+	}
+
+	nav a {
+		display: flex;
+		height: 100%;
+		align-items: center;
+		padding: 0 0.5rem;
+		color: var(--color-text);
+		font-weight: 700;
+		font-size: 0.8rem;
+		text-transform: uppercase;
+		letter-spacing: 0.1em;
+		text-decoration: none;
+		transition: color 0.2s linear;
+	}
+
+	a:hover {
+		color: var(--color-theme-1);
+	}
+</style>
