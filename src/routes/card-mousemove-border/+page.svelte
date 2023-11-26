80bd48bd381ac50891e1b7bf827d886c8499e52f
LeeOnOn <v5sollee@gmail.com>
2023-11-26 18:10:13 +0800

    feat: add css card border mouve

--- src/routes/card-mousemove-border/+page.svelte
+++ src/routes/card-mousemove-border/+page.svelte
@@ -1,13 +1,26 @@
 <script>
-  import Card from './Card.svelte';
+	import Card from './Card.svelte';
 </script>
 
 <div class="list">
-  <Card />
+	<Card />
+	<Card />
+	<Card />
+	<Card />
+	<Card />
+	<Card />
+	<Card />
+	<Card />
+	<Card />
 </div>
 
 <style>
-.list {
-  display: flex;
-}
+	.list {
+		width: 800px;
+		padding: 30px;
+		display: grid;
+		gap: 15px;
+		background-color: black;
+		grid-template-columns: repeat(3, minmax(0, 1fr));
+	}
 </style>
--- src/routes/card-mousemove-border/Card.svelte
+++ src/routes/card-mousemove-border/Card.svelte
@@ -1,11 +1,67 @@
-<div className="card">
-  Card
+<script>
+	import { onMount, onDestroy } from 'svelte';
+
+	/**
+	 * @type {HTMLDivElement}
+	 */
+	let card;
+
+	/**
+	 * @param {MouseEvent} event
+	 */
+	function handleMouseMove(event) {
+		const { clientX, clientY } = event;
+		const { left, top } = card.getBoundingClientRect();
+		const x = clientX - left;
+		const y = clientY - top;
+		card.style.setProperty('--x', `${x}px`);
+		card.style.setProperty('--y', `${y}px`);
+	}
+
+	onMount(() => {
+		document.addEventListener('mousemove', handleMouseMove);
+	});
+
+	onDestroy(() => {
+		document.removeEventListener('mousemove', handleMouseMove);
+	});
+</script>
+
+<div class="cardBox" bind:this={card}>
+	<div class="card" />
 </div>
 
 <style>
-.card {
-  width: 200;
-  height: 150px;
-  background: black;
-}
+	.cardBox {
+		position: relative;
+		--x: unset;
+		--y: unset;
+	}
+	.cardBox::before {
+		position: absolute;
+		inset: -1px;
+		/* z-index: -2; */
+		width: calc(100% + 2px);
+		height: calc(100% + 2px);
+		pointer-events: none;
+		content: '';
+		background: radial-gradient(
+			300px circle at var(--x) var(--y),
+			#c33764 0,
+			#6171fe 80%,
+			transparent 100%
+		);
+		border-radius: 6px;
+		transition: background 0.2s;
+		contain: size;
+		will-change: background;
+	}
+	.card {
+		position: relative;
+		width: 100%;
+		height: 100%;
+		background: rgb(34, 32, 32);
+		border-radius: 6px;
+		overflow: hidden;
+	}
 </style>
